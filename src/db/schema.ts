bbabe0ff205d626f55de6a03f781bbe0f08d372c
Kelvin Ochubili <kelvinochubili@gmail.com>
2026-05-25 20:51:21 +0100

    feat: add connection to db pool

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,14 +1,5 @@
-import {
-	PgTable,
-	uuid,
-	varchar,
-	text,
-	timestamp,
-	boolean,
-	integer,
-	pgTable,
-} from 'drizzle-orm/pg-core';
-import { not, relations } from 'drizzle-orm';
+import { pgTable, uuid, varchar, text, timestamp } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 
 // Users Table
